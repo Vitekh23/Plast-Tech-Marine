0e73d545038a6056dc6afe4e7b399c8d4653d51f
Victor Khil <vitekman23@mail.ru>
2025-09-05 06:52:04 +0300

    2

---
+++ js/slider.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper(".card-wrapper", {
+  loop: true,
+  spaceBetween: 24,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    },
+  }
+});
